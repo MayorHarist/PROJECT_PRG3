a0f069d57dc41cff36dd3c03d8f80f77c3c894eb
DWI IRENA DAMAYANTI <0320230010@polytechnic.astra.ac.id>
2024-06-19 12:18:26 +0700

    ini masih dashboard tapi ada yg error

--- src/main/java/Sebagai/SebagaiController.java
+++ src/main/java/Sebagai/SebagaiController.java
@@ -1,16 +1,92 @@
 package Sebagai;
 
+import Dashboard.DashboardApplication;
+import LoginMahasiswa.LoginMahasiswaController;
+import LoginTendik.LoginTendikController;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class SebagaiController extends Application {
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        launch(args);
+public class SebagaiController {
+    @FXML
+    private Button btnTenagaKependidikan;
+
+    @FXML
+    private Button btnMahasiswa;
+
+    @FXML
+    private Button btnBatal;
+    @FXML
+    private AnchorPane AnchorSebagai;
+
+    @FXML
+    protected void onbtnTenagaKependidikanClick(ActionEvent event) throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader(LoginTendikController.class.getResource("LoginTendikController.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setFullScreen(true);
+            stage.setFullScreenExitHint("");
+            stage.show();
+
+            // Tutup stage sebelumnya
+            Stage previousStage = (Stage) AnchorSebagai.getScene().getWindow();
+            previousStage.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    @FXML
+    protected void onbtnMahasiswaClick(ActionEvent event) throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader(LoginMahasiswaController.class.getResource("LoginMahasiswaController.fxml"));
+            Parent root = loader.load();
 
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setFullScreen(true);
+            stage.setFullScreenExitHint("");
+            stage.show();
+
+            // Tutup stage sebelumnya
+            Stage previousStage = (Stage) AnchorSebagai.getScene().getWindow();
+            previousStage.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void onbtnBatalClick(ActionEvent event) throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader(DashboardApplication.class.getResource("DashboardApplication.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setFullScreen(true);
+            stage.setFullScreenExitHint("");
+            stage.show();
+
+            // Tutup stage sebelumnya
+            Stage previousStage = (Stage) AnchorSebagai.getScene().getWindow();
+            previousStage.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
