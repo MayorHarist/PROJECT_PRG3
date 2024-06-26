6844240a442f7a9863ff658d953ccc733944b8b1
DWI IRENA DAMAYANTI <0320230010@polytechnic.astra.ac.id>
2024-06-26 10:01:38 +0700

    baru nambahin class aja ya belum di codingin buat jepres sama pospres

---
+++ src/main/java/Master/CRUD_JenisPrestasi/InputJepresController.java
@@ -0,0 +1,16 @@
+package Master.CRUD_JenisPrestasi;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class InputJepresController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/main/java/Master/CRUD_JenisPrestasi/UDJepresController.java
@@ -0,0 +1,16 @@
+package Master.CRUD_JenisPrestasi;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UDJepresController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
